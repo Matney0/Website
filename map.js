359f5b84f5ba4c75aa3f72a95e386c27e66dad5c
Matney0 <mlmoller21@amarillocollege.com>
2025-04-24 11:34:00 -0500

    Add files via upload

---
+++ map.js
@@ -0,0 +1,10 @@
+
+//Look here for help https://github.com/TimVoAC/GoogleAPI
+function intMap() {
+
+    let displayMap = document.createElement("displayMap");
+    let routeBox = document.createElement("routeBox");
+    let displayMap2 = document.createElement("displayMap2");
+    let amarilloCollege = { lat:}
+}
+
